a640d3b3566d05eeaee6c5b07f55bec51c9dbbb2
sdrpa <sdrpa@tagtaxa.com>
2020-12-11 21:10:39 +0100

    Broadcast data

--- simconnect-udp/simconnect-udp.cpp
+++ simconnect-udp/simconnect-udp.cpp
@@ -13,6 +13,57 @@
 HANDLE hSimConnect = NULL;
 Aircraft aircraft;
 
+sockaddr_in RecvAddr;
+SOCKET SendSocket = INVALID_SOCKET;
+
+const int UPDATES_PER_SECOND = 20;
+const size_t BUFFER_SIZE = 77;
+
+// MARK: -
+
+void openUDPBroadcast(char* destAddress, int destPort) {
+    // Initialize Winsock
+    WSADATA wsaData;
+    int result = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (result != NO_ERROR) {
+        printf("WSAStartup failed with error: %d\n", result);
+        exit(1);
+    }
+    // Create a socket for sending data
+    SendSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (SendSocket == INVALID_SOCKET) {
+        printf("Socket failed with error: %ld\n", WSAGetLastError());
+        WSACleanup();
+        exit(1);
+    }
+    // Set up the RecvAddr
+    RecvAddr.sin_family = AF_INET;
+    RecvAddr.sin_port = htons(destPort);
+    RecvAddr.sin_addr.s_addr = inet_addr(destAddress);
+}
+
+void broadcast(char buffer[BUFFER_SIZE]) {
+    int result = sendto(SendSocket,
+        buffer, BUFFER_SIZE, 0, (SOCKADDR*)&RecvAddr, sizeof(RecvAddr));
+    if (result == SOCKET_ERROR) {
+        printf("sendto failed with error: %d\n", WSAGetLastError());
+        closesocket(SendSocket);
+        WSACleanup();
+        exit(1);
+    }
+}
+
+void closeUDPBrodcast() {
+    int result = closesocket(SendSocket);
+    if (result == SOCKET_ERROR) {
+        printf("closesocket failed with error: %d\n", WSAGetLastError());
+        WSACleanup();
+    }
+    WSACleanup();
+}
+
+// MARK: -
+
 void setupDataDefinitions() {
     ASSERT_SC_SUCCESS(
         SimConnect_AddToDataDefinition(hSimConnect,
@@ -44,16 +95,53 @@ void setupDataRequests() {
     );
 }
 
+void valueCopy(char *destination, float value) {
+    memcpy(destination, (char *)&value, sizeof(float));
+}
+
+// https://docs.microsoft.com/en-us/cpp/preprocessor/pack?view=msvc-160
+#pragma pack(1)
+struct AircraftStruct {
+    char prologue[5] = { 68, 65, 84, 65, 42 };
+    char row19[4] = { 19, 0, 0, 0 };
+    float heading;
+    float h2 = -999.0;
+    float h3 = -999.0;
+    float h4 = -999.0;
+    float h5 = -999.0;
+    float h6 = -999.0;
+    float h7 = -999.0;
+    float h8 = -999.0;
+    char row20[4] = { 20, 0, 0, 0 };
+    float latitude;
+    float longitude;
+    float l3 = -999.0;
+    float l4 = -999.0;
+    float l5 = -999.0;
+    float l6 = -999.0;
+    float l7 = -999.0;
+    float l8 = -999.0;
+};
+
 void didReceiveObjectData(SIMCONNECT_RECV* pData, DWORD cbData) {
     SIMCONNECT_RECV_SIMOBJECT_DATA *pObjData = reinterpret_cast<SIMCONNECT_RECV_SIMOBJECT_DATA *>(pData);
     if (pObjData->dwRequestID == REQUEST_AIRCRAFT_POSITION) {
-        aircraft = *reinterpret_cast<Aircraft *>(&pObjData->dwData);
+        aircraft = *reinterpret_cast<Aircraft*>(&pObjData->dwData);
 
-        printf("Aircraft: %lf %lf %lf\n", 
-            degrees(aircraft.heading), 
-            degrees(aircraft.latitude),
-            degrees(aircraft.longitude)
-        );
+        //printf("Aircraft: %lf %lf %lf\n", degrees(aircraft.heading), degrees(aircraft.latitude), degrees(aircraft.longitude));
+        AircraftStruct a;
+        a.heading = degrees(aircraft.heading);
+        a.latitude = degrees(aircraft.latitude);
+        a.longitude = degrees(aircraft.longitude);
+        if (BUFFER_SIZE != sizeof(a)) {
+            printf("BUFFER_SIZE != sizeof(a)");
+            exit(1);
+        }
+        char buffer[BUFFER_SIZE];
+        memcpy(buffer, (char*)&a, BUFFER_SIZE);
+        //for (int i = 0; i < 77; i++) { printf("%d, ", buffer[i]); }
+
+        broadcast(buffer);
     }
 }
 
@@ -70,7 +158,7 @@ void didReceivedOpen() {
 
 void didReceivedQuit() {
     printf("SimConnect Received Quit\n");
-
+    exit(0);
 }
 
 void CALLBACK SC_Dispatch_Handler(SIMCONNECT_RECV *pData, DWORD cbData, void *pContext) {
@@ -99,6 +187,7 @@ void CALLBACK SC_Dispatch_Handler(SIMCONNECT_RECV *pData, DWORD cbData, void *pC
 }
 
 void runLoop() {
+    printf("Waiting for MSFS...\n");
     while (SimConnect_Open(&hSimConnect, "SimConnect UDP Broadcast", NULL, 0, 0, 0) != S_OK);
     printf("Connected\n");
 
@@ -106,6 +195,7 @@ void runLoop() {
     setupDataRequests();
 
     while (true) {
+        Sleep(1000 / UPDATES_PER_SECOND);
         SimConnect_CallDispatch(hSimConnect, SC_Dispatch_Handler, NULL);
     }
     SimConnect_Close(hSimConnect);
@@ -114,13 +204,14 @@ void runLoop() {
 int main(int argc, char *argv[]) {
     if (argc != 3) {
         printf("Usage:\n");
-        printf("\tsimconnect-udp 192.168.0.19 49003\n");
+        printf("\tsimconnect-udp <IP_ADDRESS> <PORT>\n");
         return 1;
     }
-    char *ipAddress = argv[1];
-    auto *port = argv[2];
-    printf("IP: %s Port: %s\n", ipAddress, port);
+    char *destAddress = argv[1];
+    char *destPort = argv[2];
+    printf("IP: %s Port: %d\n", destAddress, atoi(destPort));
 
-    //runLoop();
+    openUDPBroadcast(destAddress, atoi(destPort));
+    runLoop();
     return 0;
 }
--- simconnect-udp/util.h
+++ simconnect-udp/util.h
@@ -22,3 +22,10 @@ constexpr double degrees(double rad) {
 constexpr double sign(double val) {
    return (val > 0) ? 1.0 : -1.0;
 }
+
+float randomFloat(float a, float b) {
+    float random = ((float)rand()) / (float)RAND_MAX;
+    float diff = b - a;
+    float r = random * diff;
+    return a + r;
+}
